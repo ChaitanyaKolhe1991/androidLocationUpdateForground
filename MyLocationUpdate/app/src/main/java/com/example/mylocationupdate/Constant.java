bf53bf752300d47b533a111c3cd4ab7fc46ec251
chaitanya <chaitanya.kolhe@cloudganga.com>
2020-09-05 16:03:57 +0530

    Initial Commit

---
+++ MyLocationUpdate/app/src/main/java/com/example/mylocationupdate/Constant.java
@@ -0,0 +1,8 @@
+package com.example.mylocationupdate;
+
+public class Constant {
+
+    public static int LOCATION_SERVICE_ID = 175;
+    static String AC_START_LOC = "startLocationService";
+    static String AC_STOP_LOC = "stopLocationService";
+}
---
+++ MyLocationUpdate/app/src/main/java/com/example/mylocationupdate/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.mylocationupdate;
+
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+    static int REQ_LOC_PER = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQ_LOC_PER
+                    );
+                } else {
+                    startLocationService();
+                }
+            }
+        });
+        findViewById(R.id.button_stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopLocationService();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQ_LOC_PER && grantResults.length > 0) {
+            startLocationService();
+        } else {
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    boolean isLocationServiceRunning() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        if (activityManager != null) {
+            for (ActivityManager.RunningServiceInfo serviceInfo :
+                    activityManager.getRunningServices(Integer.MAX_VALUE)) {
+                if (LocationService.class.getName().equals(serviceInfo.service.getClassName())) {
+                    if (serviceInfo.foreground) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
+    void startLocationService() {
+        if (!isLocationServiceRunning()) {
+            Intent intent = new Intent(getApplicationContext(), LocationService.class);
+            intent.setAction(Constant.AC_START_LOC);
+            startService(intent);
+            Toast.makeText(this, "Location Service Started", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void stopLocationService() {
+        if (isLocationServiceRunning()) {
+            Intent intent = new Intent(getApplicationContext(), LocationService.class);
+            intent.setAction(Constant.AC_STOP_LOC);
+            startService(intent);
+            Toast.makeText(this, "Location Service Stopped", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
